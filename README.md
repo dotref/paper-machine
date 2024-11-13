56142dd4e431368ebdea83af1acdac65f1e4c63e
beepo34 <usmiaileen@gmail.com>
2024-11-12 18:48:03 -0800

    Add RAG workflow

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # paper-machine
-## rag-skeleton setup
+## rag-skeleton
+- `main.py` contains a RAG workflow that supports the following actions:
+1. adding documents to the index.
+2. removing documents from the index.
+3. using the index and a query to retrieve relevant sentences.
+
+## setup
 
 1. install dependencies
 - `python3 -m pip install -r requirements.txt`
@@ -7,5 +13,9 @@
 - `python3 data.py` will populate the `./data` folder with text files.
 3. run
 - make sure OpenAI API key is set: `os.environ['OPENAI_API_KEY'] = "sk-..."`
-- `python3 main.py` will start a command line interface to input queries. The program will output relevant chunks of text along with their file path and relvancy score. 
-- Type `q` to quit the program.
+- in the `test()` method, fill out the absolute filepath(s) of documents you wish to delete.
+- `python3 main.py` run an example sequence of actions. It will
+    - insert documents from the `./data` directory into the index.
+    - run a query, printing the retrieved sentences and their retrieval scores.
+    - remove the specified documents from the index.
+    - run the query again (deleting documents relevant to the query should cause retrieval scores to go down).
--- main.py
+++ main.py
@@ -1,34 +1,139 @@
 import os
 
+os.environ['OPENAI_API_KEY'] = "sk-..." # set OpenAI API key
 
-from llama_index.core import SimpleDirectoryReader, VectorStoreIndex
-from llama_index.core import Settings
+from llama_index.core import SimpleDirectoryReader, VectorStoreIndex, StorageContext
 from llama_index.llms.openai import OpenAI
-
-documents = SimpleDirectoryReader("./data").load_data()
-
-Settings.chunk_size = 512
-Settings.chunk_overlap = 50
-
-index = VectorStoreIndex.from_documents(
-    documents,
+from llama_index.core.node_parser import SentenceWindowNodeParser
+from llama_index.core.postprocessor import MetadataReplacementPostProcessor
+from llama_index.core.workflow import (
+    Context,
+    Workflow,
+    StartEvent,
+    StopEvent,
+    Event,
+    step,
 )
+from llama_index.core.schema import NodeWithScore
 
-def retrieve_chunks(query):
-    retriever = index.as_retriever(similarity_top_k=5)
-    results = retriever.retrieve(query)
-    return results
+class RetrieverEvent(Event):
+    nodes: list[NodeWithScore]
 
-def retrieve(query):
-    results = retrieve_chunks(query)
-    for result in results:
-        print(result.node.metadata["file_path"])
-        print(result.node)
-        print(result.score)
+class RAGWorkflow(Workflow):
+    def __init__(self):
+        # TODO: implement vector store
+
+        # create sentence window node parser
+        self.node_parser = SentenceWindowNodeParser.from_defaults(
+            window_size=3,
+            window_metadata_key="window",
+            original_text_metadata_key="original_text",
+        )
+
+        # create vector store index
+        self.index = VectorStoreIndex([])
+
+        # create set of document ids
+        self.documents = set()
+
+        super().__init__()
+
+    @step
+    async def insert(self, ctx: Context, event: StartEvent) -> StopEvent | None:
+        directory = event.get("directory")
+        if not directory:
+            return None
+        
+        # load txt files from data folder
+        documents = SimpleDirectoryReader(directory, filename_as_id=True).load_data()
+
+        # parse documents using node parser
+        nodes = self.node_parser.get_nodes_from_documents(documents)
+
+        # add document ids to set
+        for doc in documents:
+            self.documents.add(doc.doc_id)
+        
+        print(self.documents)
+
+        # add nodes to index
+        self.index.insert_nodes(nodes)
+        print("Documents inserted into index.")
+
+        return StopEvent()
+    
+    @step
+    async def delete(self, ctx: Context, event: StartEvent) -> StopEvent | None:
+        filenames = event.get("filenames")
+        if not filenames:
+            return None
+        
+        # delete nodes corresponding to documents
+        for filename in filenames:
+            if filename not in self.documents:
+                print(f"Document {filename} not found in index.")
+                continue
+            await self.index.adelete_ref_doc(filename)
+            self.documents.remove(filename)
+            print(f"Document {filename} deleted from index.")
+        
+        return StopEvent()
+
+    @step
+    async def retrieve(self, ctx: Context, event: StartEvent) -> RetrieverEvent | None:
+        query = event.get("query")
+        if not query:
+            return None
+        
+        # set query in global context
+        await ctx.set("query", query)
+
+        # create retriever
+        retriever = self.index.as_retriever(
+            similarity_top_k=5,
+            node_postprocessors=[
+                MetadataReplacementPostProcessor(target_metadata_key="window")
+            ],
+        )
+
+        # retrieve nodes
+        nodes = await retriever.aretrieve(query)
+        print(f"Retrieved {len(nodes)} nodes.")
+
+        return RetrieverEvent(nodes=nodes)
+    
+    @step
+    async def generate(self, ctx: Context, event: RetrieverEvent) -> StopEvent:
+        query = await ctx.get("query", default=None)
+        nodes = event.nodes
+
+        # TODO: generate llm response
+
+        return StopEvent(result=nodes)
+
+async def test():
+    workflow = RAGWorkflow()
+
+    # insert documents
+    await workflow.run(directory="./data")
+
+    # retrieve nodes
+    results = await workflow.run(query="What is Tesla model S?")
+
+    for res in results:
+        print(res.node.metadata["original_text"])
+        print(res.score)
+
+    # delete documents
+    await workflow.run(filenames=["FILE_PATH_HERE"]) # list absolute paths of documents to delete
+
+    # retrieve nodes
+    results = await workflow.run(query="What is Tesla model S?")
+
+    for res in results:
+        print(res.node.metadata["original_text"])
+        print(res.score)
 
 if __name__ == "__main__":
-    while True:
-        query = input("Enter a query ('q' to quit): ")
-        if query == "q":
-            break
-        retrieve(query)
+    import asyncio
+    asyncio.run(test())
