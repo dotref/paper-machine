77929b92c9ee07155f00c61ba9007fdf7a95840a
Randy Huang <huangyulu.zh@gmail.com>
2025-02-28 00:07:21 -0800

    add full v height sidebar

--- frontend/src/components/main-layout.tsx
+++ frontend/src/components/main-layout.tsx
@@ -10,11 +10,21 @@ interface MainLayoutProps {
 
 export default function MainLayout({ children, selectedTab, onSelectTab }: MainLayoutProps) {
 	return (
-		<div className="flex flex-col h-screen">
-			<MenuBar />
-			<div className="flex flex-grow">
-				<Sidebar selectedTab={selectedTab} onSelectTab={onSelectTab} />
-				<div className="flex-grow p-4 overflow-auto">
+		<div className="flex flex-col h-screen overflow-hidden">
+			{/* Fixed top menu bar */}
+			<div className="flex-shrink-0 z-10">
+				<MenuBar />
+			</div>
+			
+			{/* Main content area with sidebar */}
+			<div className="flex flex-grow overflow-hidden">
+				{/* Sidebar with full height */}
+				<div className="h-full" style={{ minHeight: "calc(100vh - 64px)" }}>
+					<Sidebar selectedTab={selectedTab} onSelectTab={onSelectTab} />
+				</div>
+				
+				{/* Content area with scrolling */}
+				<div className="flex-grow overflow-auto p-4">
 					{children}
 				</div>
 			</div>
